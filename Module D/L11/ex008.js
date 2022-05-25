d6f82b2f5e7fc63bf32f2fb830c606d9df6da3f8
Eduardo L. Oliani <olianiedu@gmail.com>
2022-05-25 17:26:36 -0300

    Adde ex008 to ex010

---
+++ Module D/L11/ex008.js
@@ -0,0 +1,10 @@
+console.log("Hello Word!") /** This is a log print **/
+var value = 100.1
+
+if (value > 50){
+    console.log("Value above 50")
+} else if (value == 50){
+    console.log("Value is 50")
+} else {
+    console.log("Value under 50")
+}
---
+++ Module D/L11/ex009.js
@@ -0,0 +1,8 @@
+var country = 'EUA'
+console.log(`Actual ${country}!`)
+
+if (country == 'BR'){
+    console.log('U are from BR')
+} else {
+    console.log('U are not BR')
+}
